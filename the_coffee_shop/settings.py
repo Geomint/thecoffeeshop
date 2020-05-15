4c760b5cb5c0ab316a6bfc311e0beafe179c9203
George <george.pyott@googlemail.com>
2020-05-15 16:50:12 +0100

    Moved secret key to env file and imported env + dj_database_url

--- the_coffee_shop/settings.py
+++ the_coffee_shop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+alc-^t5i=!jvg#su_^w1l=16a5vqil(8h(ol&mic^xwvy8sb&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
