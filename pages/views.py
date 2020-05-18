36d51c5a58f5e630585097adb7badfa6259449bf
George <george.pyott@googlemail.com>
2020-05-18 14:02:26 +0100

    Created index view in pages/views.py

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    Returns the index.html home page
+    """
+    return render(request, "index.html")
