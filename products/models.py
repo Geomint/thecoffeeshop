3f1497052046d95e721d6553bb2c7e361d7d90f1
George <george.pyott@googlemail.com>
2020-05-19 16:20:25 +0100

    Created Product model in models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    """
+    Model for products
+    """
+    name = models.CharField(max_length=254, default='')
+    grind_size = models.CharField(max_length=30, default='')
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
