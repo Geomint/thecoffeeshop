69289dd6b8cdeeb378f5d80bbb1331b7abc765e1
George <george.pyott@googlemail.com>
2020-05-20 15:27:37 +0100

    Imported reverse from django.urls in models.py

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,6 +12,6 @@ class Product(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     price = models.DecimalField(max_digits=6, decimal_places=2)
-
+    
     def __str__(self):
         return self.name
