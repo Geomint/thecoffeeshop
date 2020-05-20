9f87d625bb1fbc0fb0ed70035eeb3523ece84ee1
George <george.pyott@googlemail.com>
2020-05-20 15:28:56 +0100

    Added comma after url in products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from .views import view_all_products
 
 urlpatterns = [
-    url(r'^$', view_all_products, name='View All Products')
+    url(r'^$', view_all_products, name='View All Products'),
 ]
