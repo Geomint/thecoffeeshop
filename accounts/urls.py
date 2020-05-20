48bc2cfeb402a87ee6a7f61a43f267f136b5bee4
George <george.pyott@googlemail.com>
2020-05-20 19:27:09 +0100

    Created urls for account navigation in urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+from .views import register_account_view
+
+urlpatterns = [
+    url(r'register/', register_account_view, name="register"),
+    url(r'login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name="login"),
+    url(r'logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
+]
