dc436ec82ca9e8500e589ae647e3238d0ca8f274
George <george.pyott@googlemail.com>
2020-05-20 19:26:52 +0100

    Created register account view in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterNewUserForm
 
 # Create your views here.
+
+
+def register_account_view(request):
+    """
+    A view that registers a new account into the database
+    """
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = RegisterNewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, "You have successfully created an account, log-in now!")
+            return redirect('login')
+    else:
+        form = RegisterNewUserForm()
+    return render(request, 'register.html', context={"form": form})
