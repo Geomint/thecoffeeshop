14777d699cda7f488b1c9e4214f2afbae8650e79
George <george.pyott@googlemail.com>
2020-05-20 15:28:43 +0100

    Imported DetailView and added docstrings in views.py

--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404, reverse
 from .models import Product
 
 # Create your views here.
 def view_all_products(request):
+    """
+    renders all products on all-products page
+    """
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
