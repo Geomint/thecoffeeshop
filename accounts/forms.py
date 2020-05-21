b8b2a1701d36b3bace8fcc488303e4c0336647cf
George <george.pyott@googlemail.com>
2020-05-21 15:50:56 +0100

    Added clean_ functions in forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class RegisterNewUserForm(UserCreationForm):
     """
@@ -11,3 +12,25 @@ class RegisterNewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u'Email address must be unique')
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or not password2:
+            raise ValidationError("Confirm password")
+
+        if password1 != password2:
+            raise ValidationError("Passwords must match")
+        return password2
+
+class LoginUserForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
