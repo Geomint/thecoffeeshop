440ca711b1bcf2655653726dba62a4aabf3d23ae
George <george.pyott@googlemail.com>
2020-05-25 17:20:17 +0100

    Added add to cart url in urls.py in cart app

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import cart_contents_view
+from .views import cart_contents_view, add_to_cart_view
 
 urlpatterns = [
-    url(r'^$', cart_contents_view, name='cart'),
+    url(r'^view/', cart_contents_view, name='cart'),
+    url(r'^add/(?P<id>\d+)', add_to_cart_view, name='addtocart')
 ]
