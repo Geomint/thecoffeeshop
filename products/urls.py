683ed2c9f6af18093db1407d7fd2b2437d0734ee
George <george.pyott@googlemail.com>
2020-05-25 10:15:30 +0100

    Updated products/urls.py to include product detail page by id

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import view_all_products
+from .views import view_all_products, view_product_detail
 
 urlpatterns = [
     url(r'^$', view_all_products, name='View All Products'),
+    url(r'(?P<id>\d+)', view_product_detail, name='view_product_detail')
 ]
