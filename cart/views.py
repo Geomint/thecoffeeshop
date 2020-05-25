b8d8b5786451ee3375d6849977252190738e4e98
George <george.pyott@googlemail.com>
2020-05-25 18:34:21 +0100

    Updated add-to-cart-view and updated redirect reverse

--- cart/views.py
+++ cart/views.py
@@ -2,18 +2,38 @@ from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
+
 def cart_contents_view(request):
     """
     View that renders the cart page
     """
     return render(request, "cart.html")
 
+
 def add_to_cart_view(request, id):
     """
-    view that allows user to add items to cart
+    View that allows user to add items to cart
     """
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
     request.session['cart'] = cart
-    return redirect (reverse('home'))
+    return redirect(reverse('home'))
+
+
+def adjust_cart_view(request, id):
+    """
+    View that allows the user to alter the quantity of items in cart
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
