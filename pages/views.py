b1db321c2f985147e415dcc81643c191982b23f5
George <george.pyott@googlemail.com>
2020-05-25 19:11:14 +0100

    Created contact_us_view in views.py

--- pages/views.py
+++ pages/views.py
@@ -13,9 +13,14 @@ def index(request):
 
 def about_view(request):
     """
-    Returns the index.html home page
+    Returns the about_us.html page
     """
     page_title = 'About Us'
     return render(request, "about_us.html", {'page_title': page_title})
 
-
+def contact_us_view(request):
+    """
+    Returns the contact_us.html page and allows user to post contact forms
+    """
+    page_title = 'Contact Us'
+    return render(request, "contact_us.html", {'page_title': page_title})
