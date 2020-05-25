fa29f5eacaf6788a05697a8aa2ee079b1f9d26f0
George <george.pyott@googlemail.com>
2020-05-25 10:15:42 +0100

    Created view_product_detail view for products

--- products/views.py
+++ products/views.py
@@ -1,11 +1,17 @@
-from django.views.generic import DetailView
 from django.shortcuts import render, get_object_or_404, reverse
 from .models import Product
 
 # Create your views here.
+
+
 def view_all_products(request):
     """
     renders all products on all-products page
     """
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+
+def view_product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, 'product_detail.html', {'product': product})
