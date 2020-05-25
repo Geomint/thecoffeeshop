0289d352b1eae117446e8468b7b29ac0a0a5f502
George <george.pyott@googlemail.com>
2020-05-25 16:45:03 +0100

    Created cart_contents_view in cart/views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def cart_contents_view(request):
+    """
+    View that renders the cart page
+    """
+    return render(request, "cart.html")
