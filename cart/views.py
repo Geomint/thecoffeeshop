ee908ff50ea64b8b8e9a00c34a59ee59e92d70d8
George <george.pyott@googlemail.com>
2020-05-25 17:20:28 +0100

    Created add-to-cart-view in views.py for cart app

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -7,3 +7,13 @@ def cart_contents_view(request):
     View that renders the cart page
     """
     return render(request, "cart.html")
+
+def add_to_cart_view(request, id):
+    """
+    view that allows user to add items to cart
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+    request.session['cart'] = cart
+    return redirect (reverse('home'))
