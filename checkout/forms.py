a0f2da26037e0b1f9e3f58858259a9fb031b3c1e
George <george.pyott@googlemail.com>
2020-05-26 19:40:14 +0100

    Created OrderForm form in checkout/forms.py

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,12 +1,32 @@
 from django import forms
 from .models import Order
 
+
 class PaymentForm(forms.form):
+    """
+    Form to allow user to enter in card details to make payment
+    """
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOCIES = [(i, i) for i in range(2018, 2040)]
 
     card_number = forms.CharField(label='Card Number', required=True)
     cvv = forms.CharField(label='CVV', requrired=True)
-    expiry_month = forms.ChoiceField(label='Expiry Month', choices=MONTH_CHOICES, required=True)
-    expiry_year = forms.ChoiceField(label='Expiry Year', choices=YEAR_CHOCIES, required=True)
+    expiry_month = forms.ChoiceField(
+        label='Expiry Month', choices=MONTH_CHOICES, required=True)
+    expiry_year = forms.ChoiceField(
+        label='Expiry Year', choices=YEAR_CHOCIES, required=True)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+
+class OrderForm(forms.form):
+    class Meta:
+        model = Order
+        fields = (
+            'full_name',
+            'phone_number',
+            'country',
+            'postcode',
+            'city',
+            'address_line_1',
+            'address_line_2',
+        )
