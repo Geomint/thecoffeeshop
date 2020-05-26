bf3ef70ce41b25987bf245171ab85998e284dfe1
George <george.pyott@googlemail.com>
2020-05-26 10:06:57 +0100

    Created search view to return filtered products from DB

--- search/views.py
+++ search/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
+
+def search_view(request):
+    """
+    Returns a view with a list of filtered products based on search query
+    """
+    products = Product.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "products.html", {"products": products})
