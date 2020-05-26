fd60a2c20d4e19980228db6a4670974e250eed7a
George <george.pyott@googlemail.com>
2020-05-26 16:53:04 +0100

    Created jquery to handle show hide of mobile-nav

--- static/js/main-nav.js
+++ static/js/main-nav.js
@@ -8,7 +8,15 @@ searchButton.on('click', function(){
     overlay.addClass('overlay__show');
 });
 
+$('.nav-button').on('click', function () {
+    overlay.addClass('overlay__show');   
+    $('html').addClass('open-nav');
+});
+
 overlay.on('click', function(){
     $('.search').addClass('search__hide');
     overlay.removeClass('overlay__show');
+    $('html').removeClass('open-nav');
 })
+
+
