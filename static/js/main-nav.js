e1f4ee6796975e41e8f7645f4313eadd178c88d1
George <george.pyott@googlemail.com>
2020-05-26 10:07:10 +0100

    Included click to close jquery for search + overlay

--- static/js/main-nav.js
+++ static/js/main-nav.js
@@ -1,8 +1,14 @@
 // main-nav js goes here
 
 const searchButton = $('#search-button');
+const overlay = $('#overlay')
 
 searchButton.on('click', function(){
     $('.search').removeClass('search__hide');
-    $('#overlay').addClass('overlay__show');
+    overlay.addClass('overlay__show');
+});
+
+overlay.on('click', function(){
+    $('.search').addClass('search__hide');
+    overlay.removeClass('overlay__show');
 })
