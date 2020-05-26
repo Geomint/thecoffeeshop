62cd4acb74b068cdce88af6c511b6d667a4fed82
George <george.pyott@googlemail.com>
2020-05-26 19:18:29 +0100

    Created order model in checkout/models.py

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=40, blank=False)
+    city = models.CharField(max_length=40, blank=False)
+    address_line_1 = models.CharField(max_length=40, blank=False)
+    address_line_2 = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.id}-{self.date}-{self.full_name}'
+
