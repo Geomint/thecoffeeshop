3f14d5f812c3838d2799eeeccdabd8ad713d3d8d
George <george.pyott@googlemail.com>
2020-05-26 19:37:42 +0100

    Created PaymentForm in checkout/forms.py

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,2 +1,12 @@
 from django import forms
 from .models import Order
+
+class PaymentForm(forms.form):
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOCIES = [(i, i) for i in range(2018, 2040)]
+
+    card_number = forms.CharField(label='Card Number', required=True)
+    cvv = forms.CharField(label='CVV', requrired=True)
+    expiry_month = forms.ChoiceField(label='Expiry Month', choices=MONTH_CHOICES, required=True)
+    expiry_year = forms.ChoiceField(label='Expiry Year', choices=YEAR_CHOCIES, required=True)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
