6cc98ee9c8c951b3c0bd2882e23f0562cb2aa9ce
George <george.pyott@googlemail.com>
2020-05-27 10:08:16 +0100

    Imported necessary items into views.py in chcekout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import OrderForm, PaymentForm
+from .models import OrderItem
+from django.conf import settings
+from django.utils import timezone
+from products.models import Product
+import stripe
 
 # Create your views here.
