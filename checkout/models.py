f6838d8d7948d772c377fd31f950b412c9e27fb9
George <george.pyott@googlemail.com>
2020-05-27 20:40:59 +0100

    Added user field to Order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from products.models import Product
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 
 class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
@@ -15,7 +18,7 @@ class Order(models.Model):
     date = models.DateField()
 
     def __str__(self):
-        return f'{self.id}-{self.date}-{self.full_name}'
+        return f'{self.id}-{self.date}-{self.user.id}'
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, null=False)
