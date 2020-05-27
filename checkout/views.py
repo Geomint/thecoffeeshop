cf6c9ab27486bb6eb82085c6d6eba2c199619c96
George <george.pyott@googlemail.com>
2020-05-27 20:40:41 +0100

    Added order.user to checkout/views.py

--- checkout/views.py
+++ checkout/views.py
@@ -22,6 +22,7 @@ def checkout_view(request):
 
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
+            order.user = request.user
             order.date = timezone.now()
             order.save()
 
