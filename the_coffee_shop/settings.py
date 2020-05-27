19267974517f07385afc121bef7168cf46bc48a7
George <george.pyott@googlemail.com>
2020-05-27 12:07:45 +0100

    Included Stripe keys in settings.py

--- the_coffee_shop/settings.py
+++ the_coffee_shop/settings.py
@@ -136,6 +136,9 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
