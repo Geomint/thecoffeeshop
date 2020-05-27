9b9f01dd1014f5eddced4525db091749db9c19ab
George <george.pyott@googlemail.com>
2020-05-27 15:48:20 +0100

    Beautify checkout/forms.py

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,15 +7,15 @@ class PaymentForm(forms.Form):
     Form to allow user to enter in card details to make payment
     """
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
-    YEAR_CHOCIES = [(i, i) for i in range(2018, 2040)]
+    YEAR_CHOICES = [(i, i) for i in range(2018, 2040)]
 
-    card_name = forms.CharField(label='Card Name', required=True)
-    card_number = forms.CharField(label='Card Number', required=True)
-    cvv = forms.CharField(label='CVV', required=True)
+    card_number = forms.CharField(
+        label='Credit card number', required=False)
+    cvv = forms.CharField(label='Security code (CVV)', required=False)
     expiry_month = forms.ChoiceField(
-        label='Expiry Month', choices=MONTH_CHOICES, required=True)
+        label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(
-        label='Expiry Year', choices=YEAR_CHOCIES, required=True)
+        label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
