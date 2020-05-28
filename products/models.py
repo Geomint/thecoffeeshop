e8d910bccb798e1a8cb1b7acf5b8c300e5634375
George <george.pyott@googlemail.com>
2020-05-28 11:09:01 +0100

    Added grind_options to product model and updated grind_size field

--- products/models.py
+++ products/models.py
@@ -3,15 +3,36 @@ from django.urls import reverse
 
 # Create your models here.
 
+
 class Product(models.Model):
     """
     Model for products
     """
+    EXTRA_COARSE = 'Extra Coarse'
+    COARSE = 'Coarse'
+    MEDIUM_COARSE = 'Medium Coarse'
+    MEDIUM = 'Medium'
+    MEDIUM_FINE = 'Medium Fine'
+    FINE = 'Fine'
+    VERY_FINE = 'Very Fine'
+    GRIND_OPTIONS = [
+        (EXTRA_COARSE, 'Extra Coarse'),
+        (COARSE, 'Coarse'),
+        (MEDIUM_COARSE, 'Medium Coarse'),
+        (MEDIUM, 'Medium'),
+        (MEDIUM_FINE, 'Medium Fine'),
+        (FINE, 'Fine'),
+        (VERY_FINE, 'Very Fine'),
+    ]
     name = models.CharField(max_length=254, default='')
-    grind_size = models.CharField(max_length=30, default='')
+    grind_size = models.CharField(
+        max_length=20,
+        choices=GRIND_OPTIONS,
+        default=FINE,
+    )
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    
+
     def __str__(self):
         return self.name
