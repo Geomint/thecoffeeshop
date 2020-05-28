82b563e98ff623e9c77edf1248736508ff110394
George <george.pyott@googlemail.com>
2020-05-28 12:33:45 +0100

    Added jquery to handle show/hide of extra description

--- static/js/main-nav.js
+++ static/js/main-nav.js
@@ -3,20 +3,35 @@
 const searchButton = $('#search-button');
 const overlay = $('#overlay')
 
-searchButton.on('click', function(){
+searchButton.on('click', function () {
     $('.search').removeClass('search__hide');
     overlay.addClass('overlay__show');
 });
 
 $('.nav-button').on('click', function () {
-    overlay.addClass('overlay__show');   
+    overlay.addClass('overlay__show');
     $('html').addClass('open-nav');
 });
 
-overlay.on('click', function(){
+overlay.on('click', function () {
     $('.search').addClass('search__hide');
     overlay.removeClass('overlay__show');
     $('html').removeClass('open-nav');
-})
+});
 
+const readMore = $("#read-more");
+const readLess = $("#read-less");
+const descriptionTwo = $("#more-description");
+const speed = 200;
 
+readMore.on('click', function () {
+    descriptionTwo.show(speed);
+    readMore.hide(speed);
+    return false;
+});
+
+readLess.on('click', function () {
+    descriptionTwo.hide(speed);
+    readMore.show(speed);
+    return false;
+});
