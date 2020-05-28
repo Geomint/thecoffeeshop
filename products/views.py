d71a82cbe28ac35fc75bb29fa94f7bf4eb087eb9
George <george.pyott@googlemail.com>
2020-05-28 20:45:37 +0100

    Included pagination for view_all_products view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 
 # Create your views here.
@@ -9,6 +10,14 @@ def view_all_products(request):
     renders all products on all-products page
     """
     products = Product.objects.all()
+    paginator = Paginator(products, 6)  # show 6 products per page
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "products.html", {"products": products})
 
 
