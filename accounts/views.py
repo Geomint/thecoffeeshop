e8b3e2199bb7e7ebd52323e7f735a99fcf87e323
George <george.pyott@googlemail.com>
2020-05-28 13:14:46 +0100

    Created upload_product_view in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from checkout.models import OrderItem
 from .forms import RegisterNewUserForm, LoginUserForm, UpdateUserDetailsForm, UploadProductForm
 
 # Create your views here.
@@ -55,25 +56,34 @@ def account_profile_view(request):
     """
     A view that returns the logged-in users profile page
     """
+    orders = OrderItem.objects.all()
     user = User.objects.get(email=request.user.email)
     if request.method == 'POST':
-        update_form = UpdateUserDetailsForm(request.POST, instance=request.user, prefix='banned')
+        update_form = UpdateUserDetailsForm(request.POST, instance=request.user)
         if update_form.is_valid():
             update_form.save()
             messages.success(
                 request, 'You have successfully updated your account details.')
             return redirect('profile')
     else: 
-        update_form = UpdateUserDetailsForm(instance=request.user, prefix='banned')
+        update_form = UpdateUserDetailsForm(instance=request.user)
     
-    if request.method == 'POST' and not update_form.is_valid():
-        product_upload_form = UploadProductForm(request.POST, prefix='expected')
-        update_form = UpdateUserDetailsForm(prefix='banned')
-        if product_upload_form.is_valid():
-            product_upload_form.save()
-            messages.success(request, 'You have successfully uploaded a product to the DataBase')
+    return render(request, 'profile_page.html', {"profile": user, "update_form": update_form, "orders": orders})
+
+@login_required
+def upload_product_view(request):
+    """
+    A view that allows users flagged as staff or admin to upload products from profile page
+    """
+    user = User.objects.get(email=request.user.email)
+    if request.method == 'POST':
+        product_upload = UploadProductForm(request.POST, instance=request.user)
+        if product_upload.is_valid():
+            product_upload.save()
+            messages.success(
+                request, 'You have successfully uploaded this product.')
             return redirect('profile')
     else: 
-        product_upload_form = UploadProductForm(prefix='expected')
-
-    return render(request, 'profile_page.html', {"profile": user, "update_form": update_form, "product_upload_form": product_upload_form})
+        product_upload = UploadProductForm(instance=request.user)
+    
+    return render(request, 'profile_page.html', {"profile": user, "product_upload": product_upload})
