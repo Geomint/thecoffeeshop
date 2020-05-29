9f7267ace0824d1dbcbabc1418c5752c5d9851e9
George <george.pyott@googlemail.com>
2020-05-29 07:37:17 +0100

    Removed ENV import

--- the_coffee_shop/settings.py
+++ the_coffee_shop/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -136,7 +135,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
