6f538de00906113220ceab7590078b576921bed9
George <george.pyott@googlemail.com>
2020-05-31 14:11:02 +0100

    Return no results page if query doesnt find products

--- search/views.py
+++ search/views.py
@@ -3,9 +3,15 @@ from products.models import Product
 
 # Create your views here.
 
+
 def search_view(request):
     """
     Returns a view with a list of filtered products based on search query
     """
     products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+    if products.exists():
+        return render(request, "products.html", {"products": products})
+    else:
+        search_query = request.GET['q']
+        products = Product.objects.all()
+        return render(request, "no-results.html", {"products": products, "search_query": search_query})
