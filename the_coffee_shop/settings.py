465ae52276ed1e03a529c1d413e7e00fbcf93705
George <george.pyott@googlemail.com>
2020-05-31 14:37:14 +0100

    Re-added media settings to settings.py

--- the_coffee_shop/settings.py
+++ the_coffee_shop/settings.py
@@ -137,6 +137,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
