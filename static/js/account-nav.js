93be649477179a675e7fccf3c6e0d1990cffb2c9
George <george.pyott@googlemail.com>
2020-05-31 14:14:58 +0100

    Removed jquery to handle show/hide of product upload form

--- static/js/account-nav.js
+++ static/js/account-nav.js
@@ -1,31 +1,20 @@
 // account navigation js goes here 
 const accountNavButton = $('.profile__navigation__link__account');
 const orderNavButton = $('.profile__navigation__link__orders');
-const productNavButton = $('.profile__navigation__link__product-upload');
 const accountPanel = $('.profile__dashboard__account');
 const ordersPanel = $('.profile__dashboard__orders');
-const productPanel = $('.profile__dashboard__product');
 const editProfileTrigger = $('.edit-profile-trigger');
 const editProfileClose = $('.edit-profile-close');
 
 accountNavButton.on('click', function(){
     accountPanel.show();
     ordersPanel.hide();
-    productPanel.hide();
     return false;
 });
 
 orderNavButton.on('click', function(){
     accountPanel.hide();
     ordersPanel.show();
-    productPanel.hide();
-    return false;
-});
-
-productNavButton.on('click', function(){
-    accountPanel.hide();
-    ordersPanel.hide();
-    productPanel.show();
     return false;
 });
 
