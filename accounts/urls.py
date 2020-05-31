1b9bdaf53d946852f9006c1a54c342fbad0bf48f
George <george.pyott@googlemail.com>
2020-05-31 14:14:37 +0100

    Removed upload product url from accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-from .views import register_account_view, login_account_view, account_profile_view, upload_product_view
+from .views import register_account_view, login_account_view, account_profile_view
 
 urlpatterns = [
     url(r'register/', register_account_view, name="register"),
     url(r'login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name="login"),
     url(r'logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
     url(r'profile/', account_profile_view, name="profile"),
-    url(r'product/upload', upload_product_view, name="productupload"),
 ]
